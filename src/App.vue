b0f5ef91fc50409db51d386f5de52905116e14ec
gastonm3112 <gastonm3112@gmail.com>
2021-01-30 15:10:12 -0300

    Header creado

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+</style>
